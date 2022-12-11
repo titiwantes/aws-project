124f02eba85b65489777c392e26a003e08a52e98
Thierry <thierry.masumbuko@epitech.eu>
2022-12-11 04:02:14 +0100

    github actions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import {Amplify} from "aws-amplify";
 import awsconfig from "./aws-exports";
 import { Authenticator, withAuthenticator } from "@aws-amplify/ui-react";
-import "./App.css";
 import '@aws-amplify/ui-react/styles.css';
-
+import Home from "./pages/Home";
 Amplify.configure(awsconfig);
 
 function App() {
   return (
     <Authenticator>
-      <div className="App">
-        <h2>APP</h2>
-      </div>
+      <Home />
     </Authenticator>
 
   );
